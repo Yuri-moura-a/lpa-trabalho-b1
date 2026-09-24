15ef2606d4a5751da8faf826ffb0aea6c3a3fb0e
Yuri-moura-a <yuri.moura@a.ucb.br>
2026-09-24 15:09:21 -0300

    entrada dos dados
    Adição do menu principal, no qual são fornecidas as entradas necessárias para o programa.

--- src/main.c
+++ src/main.c
@@ -4,7 +4,23 @@
 
 int main() {
 
-  system("chcp 65001 > nul");
+	system("chcp 65001 > nul");
 	setlocale(LC_ALL, "Portuguese");
+	
+	float km, kg;
+	int modalidade, servico, tentativa;
 
+	printf("=== MENU PRINCIPAL ===\n");
+
+	printf("Informe a distancia em quilometros: ");
+	scanf("%f", &km);
+
+	printf("Informe o peso em quilos: ");
+	scanf("%f", &kg);
+
+	printf("Informe a modalidade:\n");
+	printf("1 - Economica\n");
+	printf("2 - Expressa\n");
+	printf("3 - Prioritaria\n");
+	scanf("%d", &modalidade);
 }
